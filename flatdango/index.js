526f4ade9f2b54c1f79f33f777744ccaecd3575b
Felix <felix@DESKTOP-F0L63C2.localdomain>
2023-10-23 22:05:17 +0300

    declaring a function that stores currently displayed film data and nu…
    …mber of available tickets

--- flatdango/index.js
+++ flatdango/index.js
@@ -2,3 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const filmsList = document.getElementById('films');
     const filmDetails = document.getElementById('film-details');
     const buyTicketButton = document.getElementById('buy-ticket');
+
+    let currentFilm; 
+    let availableTickets; 
